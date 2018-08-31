35eaf684d9c54a03582b1595bcda1e0542c4c3a1
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-08-31 14:28:39 -0400

    Move Provider to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
 import TextInput from './TextInput.jsx'
+import store from '../store'
+import { Provider } from 'react-redux'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TextInput />
+        <Provider store={store}>
+          <TextInput />
+        </Provider>
       </div>
     )
   }
