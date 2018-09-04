1afc8a349e5f22da60d7d49d670f0617ce5c0a76
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-04 10:25:46 -0400

    Tests for redux store

---
+++ src/store/reducer.test.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai'
+import { reducer } from './index'
+
+describe('reducer', () => {
+  it('is a function', () => {
+    expect(reducer).to.be.a('function')
+  })
+
+  describe('when called with a state and a nonexistent action', () => {
+    it('returns the state unchanged', () => {
+      const testState = { text: 'bar baz' }
+      expect(
+        reducer(testState, { type: 'NOTHING', data: 'not a real action' })
+      ).to.equal(testState)
+    })
+  })
+
+  describe('when passed UPDATE_TEXT action', () => {
+    it('returns an object with the newText of the action as its text', () => {
+      const testState = { text: 'bar baz' }
+      expect(
+        reducer(testState, { type: 'UPDATE_TEXT', newText: 'new string' }).text
+      ).to.equal('new string')
+    })
+
+    it('copies over key/value pairs other than text unchanged', () => {
+      const testState = { text: 'bar baz', otherThing: 12 }
+      expect(
+        reducer(testState, { type: 'UPDATE_TEXT', newText: 'new string' })
+          .otherThing
+      ).to.equal(12)
+    })
+  })
+
+  describe('when passed ADD_TO_TEXT action', () => {
+    it('returns an object with the newText of the action appended to its text', () => {
+      const testState = { text: 'bar baz' }
+      expect(
+        reducer(testState, { type: 'ADD_TO_TEXT', newText: ' foo' }).text
+      ).to.equal('bar baz foo')
+    })
+
+    it('copies over key/value pairs other than text unchanged', () => {
+      const testState = { text: 'bar baz', otherThing: 12 }
+      expect(
+        reducer(testState, { type: 'ADD_TO_TEXT', newText: ' foo' }).otherThing
+      ).to.equal(12)
+    })
+  })
+})
