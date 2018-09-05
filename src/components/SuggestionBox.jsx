f6c7801fa58369fee03997a09322db7f7011732d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-05 15:01:39 -0400

    SuggestionBox adds to text

--- src/components/SuggestionBox.jsx
+++ src/components/SuggestionBox.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addToText } from '../store'
 
-const SuggestionBox = props => {
-		return (
-			<div className="suggestionBox" onClick={props.handleClick}>
-				{props.value}
-			</div>
-		)
-	}
-
-SuggestionBox.propTypes = {
-	value: PropTypes.string.isRequired,
-  handleClick: PropTypes.func.isRequired
+export const SuggestionBox = props => {
+  return (
+    <div
+      className="suggestionBox"
+      onClick={() => props.handleClick(props.value)}
+    >
+      {props.value}
+    </div>
+  )
 }
 
-export default SuggestionBox
+SuggestionBox.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+}
+
+function mapStateToProps() {
+  return {}
+}
+
+function mapDispatch(dispatch) {
+  return {
+    handleClick: function(toAdd) {
+      dispatch(addToText(toAdd))
+    },
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatch
+)(SuggestionBox)
--- src/components/SuggestionBox.test.js
+++ src/components/SuggestionBox.test.js
@@ -2,28 +2,30 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import { spy } from 'sinon'
-import SuggestionBox from './SuggestionBox.jsx'
+import { SuggestionBox } from './SuggestionBox.jsx'
 
 describe('SuggestionBox', () => {
   let suggestionBox
   let clickSpy
   beforeEach(() => {
     clickSpy = spy()
-    suggestionBox = shallow(<SuggestionBox value="foo" handleClick={clickSpy} />)
+    suggestionBox = shallow(
+      <SuggestionBox value="foo" handleClick={clickSpy} />
+    )
   })
 
-	it('renders a div with class "suggestionBox"', () => {
-		expect(suggestionBox.find('div.suggestionBox').length).to.equal(1)
-	})
+  it('renders a div with class "suggestionBox"', () => {
+    expect(suggestionBox.find('div.suggestionBox').length).to.equal(1)
+  })
 
   it('renders a div with the text from it\'s "value" prop', () => {
-		expect(suggestionBox.find('div.suggestionBox').text()).to.equal('foo')
-	})
+    expect(suggestionBox.find('div.suggestionBox').text()).to.equal('foo')
+  })
 
   describe('when clicked', () => {
     it('calls handleClick function passed as prop with value', () => {
-			suggestionBox.simulate('click')
-			expect(clickSpy.calledWith('foo'))
-		})
+      suggestionBox.simulate('click')
+      expect(clickSpy.calledWith('foo'))
+    })
   })
 })
