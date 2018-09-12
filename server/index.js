fb7670eed3e518e57a3f5b0624f20761928e3f5d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-12 10:34:18 -0400

    API route to query a WordTries

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const WordTries = require('build-word-tries')
 
 const app = express()
 const server = app.listen(8080, () => console.log(`Listening on port 8080`))
 
-app.use('/test', (req, res) => {
-  res.json({test: 'this is a test'})
+const str =
+  '"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."'
+const tries = new WordTries(str, 1)
+
+app.use('/api/testText/:word', (req, res) => {
+  if (req.params.word) {
+    const nextWords = tries.get(req.params.word)
+    res.json(nextWords)
+  } else {
+    res.json(test.get())
+  }
 })
 
 module.exports = app
