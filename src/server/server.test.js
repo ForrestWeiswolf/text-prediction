78b2d979285ac1c70391d036c64339d9ecd65a6a
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-15 10:31:02 -0400

    Remove faulty test

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -6,12 +6,6 @@ const agent = supertest.agent(server)
 
 describe('server', () => {
   describe('GET /api/corpora', () => {
-    it('jsons back an array', () => {
-      agent
-        .get('/api/corpora')
-        .expect('Content-Type', /json/)
-        .expect(res => !!res.body.length)
-        .expect(200)
-    })
+    it('sends a response', done => {})
   })
 })
