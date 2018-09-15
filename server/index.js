12cacd8922a5a43a2284241fb5fc95f1b610abc1
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-15 09:52:46 -0400

    split readAndBuildTries into own file

--- server/index.js
+++ server/index.js
@@ -1,27 +1,11 @@
 const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const WordTries = require('build-word-tries')
+const readAndBuildTries = require('./readAndBuildTries.js')
 
 const app = express()
 const server = app.listen(8080, () => console.log(`Listening on port 8080`))
 
-function readAndBuildTries(filename, depth, callback) {
-  fs.readFile(
-    path.join(__dirname, filename),
-    { encoding: 'utf-8' },
-    (err, data) => {
-      if (err) {
-        throw err
-      } else {
-        const tries = new WordTries(data, depth)
-        callback(tries)
-      }
-    }
-  )
-}
-
 readAndBuildTries('/corpora/testfile.txt', 1, test => {
+  console.log('Tries ready')
   app.use('/api/testfile/:word', (req, res) => {
     if (req.params.word) {
       const nextWords = test.get(req.params.word)
---
+++ server/readAndBuildTries.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const WordTries = require('build-word-tries')
+
+function readAndBuildTries(filename, depth, callback) {
+  fs.readFile(
+    path.join(__dirname, filename),
+    { encoding: 'utf-8' },
+    (err, data) => {
+      if (err) {
+        throw err
+      } else {
+        const tries = new WordTries(data, depth)
+        callback(tries)
+      }
+    }
+  )
+}
+
+module.exports = readAndBuildTries
