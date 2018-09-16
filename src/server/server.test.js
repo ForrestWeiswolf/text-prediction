1dfc882f4b6902a0d6379e84b50c3d3be2cb1944
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-16 11:28:02 -0400

    Revert "async test problems"
    This reverts commit 8de34116044e45c45e051c0ed4a9df43fef606d5.

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -1,4 +1,4 @@
-// const expect = require('chai').expect
+const expect = require('chai').expect
 const supertest = require('supertest')
 const server = require('./server.js')
 
@@ -7,24 +7,5 @@ const agent = supertest.agent(server)
 describe('server', () => {
   describe('GET /api/corpora', () => {
     it('sends a response', done => {})
-
-    // it('just fails', () => {
-    //   expect(1).toBe(1)
-    // })
-
-    it('just fails', done => {
-      expect.assertions(1)
-      const promise = new Promise((resolve, reject) => {
-        resolve('foo')
-      })
-
-      return promise
-        .then(res => {
-          expect(res).toEqual('bar')
-        })
-        .catch(err => {
-          throw err
-        })
-    })
   })
 })
