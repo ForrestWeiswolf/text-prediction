4c512b91146e6fa20ccc27a56b451b898592a6e7
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-18 10:40:33 -0400

    SuggestionBoxContainer calls /api/corpora/testfile

--- src/client/components/SuggestionBoxContainer.jsx
+++ src/client/components/SuggestionBoxContainer.jsx
@@ -4,12 +4,29 @@ import axios from 'axios'
 import SuggestionBox from './SuggestionBox.jsx'
 
 class SuggestionBoxContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      words: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api/corpora/testfile').then(res => {
+      this.setState({ words: res.data })
+    })
+  }
+
   render() {
-    return <div>
-      <SuggestionBox />
-      <SuggestionBox />
-      <SuggestionBox />
-    </div>
+    const words = this.state.words
+
+    return (
+      <div>
+        {words.map((word, idx) => (
+          <SuggestionBox value={word} key={idx}/>
+        ))}
+      </div>
+    )
   }
 }
 
--- src/client/components/SuggestionBoxContainer.test.js
+++ src/client/components/SuggestionBoxContainer.test.js
@@ -3,6 +3,12 @@ import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import SuggestionBox from './SuggestionBox.jsx'
 import SuggestionBoxContainer from './SuggestionBoxContainer.jsx'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+const mock = new MockAdapter(axios)
+const testReply = ['foo', 'bar', 'baz']
+mock.onGet('/api/corpora/testfile').reply(200, testReply)
 
 describe('SuggestionBoxContainer', () => {
   let suggestionBoxContainer
@@ -10,7 +16,19 @@ describe('SuggestionBoxContainer', () => {
     suggestionBoxContainer = shallow(<SuggestionBoxContainer />)
   })
 
+  afterAll(() => {
+    mock.restore()
+  })
+
   it('Renders three SuggestionBox-es', () => {
     expect(suggestionBoxContainer.find(SuggestionBox).length).to.equal(3)
+  })
+
+  it('passes them the values from a call to /api/corpora/testfile', () => {
+    const boxes = suggestionBoxContainer.find(SuggestionBox)
+
+    testReply.forEach((word, idx) => {
+      expect(boxes.at(idx).props().value).to.equal(word)
+    })
   })
 })
