77f35c9602ce697ddfdda25bf99b4aa14f7ce477
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-23 11:40:03 -0400

    SuggestionBoxContainer gets suggestions from state

--- src/client/components/SuggestionBoxContainer.jsx
+++ src/client/components/SuggestionBoxContainer.jsx
@@ -1,43 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import axios from 'axios'
 import { connect } from 'react-redux'
 import SuggestionBox from './SuggestionBox.jsx'
 
 export class SuggestionBoxContainer extends Component {
   constructor(props) {
     super()
-
-    this.state = {
-      suggestions: [],
-    }
-
-    this.isCancelled = false
-    this.fetchWords = this.fetchWords.bind(this)
-  }
-
-  componentDidMount() {
-    this.fetchWords()
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.fetchWords()
-  }
-
-  componentWillUnmount() {
-    this.isCancelled = true
-  }
-
-  fetchWords() {
-    const wordRoute = this.props.lastWord ? `/${this.props.lastWord}` : ''
-
-    return axios.get('/api/corpora/testfile' + wordRoute).then(res => {
-      this.isCancelled || this.setState({ suggestions: res.data })
-    })
   }
 
   render() {
-    const suggestions = this.state.suggestions
+    const suggestions = this.props.suggestions
 
     return (
       <div>
@@ -57,6 +29,7 @@ function mapStateToProps(state) {
   const words = state.text.split(/\W+/)
   return {
     lastWord: words[words.length - 1],
+    suggestions: state.suggestions,
   }
 }
 
--- src/client/components/SuggestionBoxContainer.test.js
+++ src/client/components/SuggestionBoxContainer.test.js
@@ -3,47 +3,25 @@ import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import SuggestionBox from './SuggestionBox.jsx'
 import { SuggestionBoxContainer } from './SuggestionBoxContainer.jsx'
-import axios from 'axios'
-import { spy } from 'sinon'
-import MockAdapter from 'axios-mock-adapter'
-
-const mock = new MockAdapter(axios)
-const testReply = ['foo', 'bar', 'baz']
-
-mock.onGet(/api\/corpora\/testfile\/?$/).reply(200, testReply)
 
 describe('SuggestionBoxContainer', () => {
   let suggestionBoxContainer
+  const testSuggestions = ['foo', 'bar', 'baz']
   beforeEach(() => {
-    suggestionBoxContainer = shallow(<SuggestionBoxContainer lastWord="" />)
-  })
-
-  afterAll(() => {
-    mock.restore()
+    suggestionBoxContainer = shallow(
+      <SuggestionBoxContainer lastWord="" suggestions={testSuggestions} />
+    )
   })
 
   it('Renders three SuggestionBox-es', () => {
     expect(suggestionBoxContainer.find(SuggestionBox).length).to.equal(3)
   })
 
-  it('passes them the values from a call to /api/corpora/testfile', () => {
+  it('passes them the values from suggestions prop', () => {
     const boxes = suggestionBoxContainer.find(SuggestionBox)
 
-    testReply.forEach((word, idx) => {
+    testSuggestions.forEach((word, idx) => {
       expect(boxes.at(idx).props().value).to.equal(word)
-    })
-  })
-
-  it('calls to /api/corpora/testfile with its lastWord prop', done => {
-    const replySpy = spy(config => [200, testReply])
-
-    mock.onGet(/api\/corpora\/testfile\/foo\/?/).reply(replySpy)
-
-    suggestionBoxContainer = shallow(<SuggestionBoxContainer lastWord="foo" />)
-
-    setImmediate(() => {
-      expect(replySpy.called).to.be.true
-      done()
     })
   })
 })
