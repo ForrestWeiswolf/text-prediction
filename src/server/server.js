e163fcddf79f95fa37b2ff7045d15c41cb41fd9b
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-24 14:09:03 -0400

    Server gets file info from list.json

--- src/server/server.js
+++ src/server/server.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const { readAndBuildTries } = require('./fsUtils.js')
+const list = require('./corpora/list.json')
 
 const app = express()
 
-const testFile = {
-  name: 'testfile',
-  location: '/corpora/testfile.txt',
-  start: 24,
-  end: null
+function createRoutes(file) {
+  readAndBuildTries(
+    `./corpora/${file.name}.txt`,
+    file.start,
+    file.end,
+    1,
+    trie => {
+      console.log(`Loaded ${file.name}`)
+
+      app.use(`/api/corpora/${file.name}/:word`, (req, res) => {
+        const nextWords = trie.get(req.params.word)
+        res.json(nextWords.slice(0, 3))
+      })
+
+      app.use(`/api/corpora/${file.name}`, (req, res) => {
+        res.json(trie.get().slice(0, 3))
+      })
+    }
+  )
 }
 
-function createRoutes(file){
-  readAndBuildTries(file.location, file.start, file.end, 1, trie => {
-    console.log(`Loaded ${file.name}`)
-
-    app.use(`/api/corpora/${file.name}/:word`, (req, res) => {
-      const nextWords = trie.get(req.params.word)
-      res.json(nextWords.slice(0, 3))
-    })
-  
-    app.use(`/api/corpora/${file.name}`, (req, res) => {
-      res.json(trie.get().slice(0, 3))
-    })  
-  })
-}
-
-createRoutes(testFile)
+list.forEach(file => {
+  createRoutes(file)
+})
 
 module.exports = app
