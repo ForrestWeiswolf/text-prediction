ff2d9d8105b698a9bdef2fb684b0f7388a20e2c9
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-24 11:28:42 -0400

    Refactor server.js

--- src/server/server.js
+++ src/server/server.js
@@ -3,17 +3,28 @@ const { readAndBuildTries } = require('./fsUtils.js')
 
 const app = express()
 
-readAndBuildTries('/corpora/testfile.txt', 24, null, 1, test => {
-  console.log('Tries ready')
+const testFile = {
+  name: 'testfile',
+  location: '/corpora/testfile.txt',
+  start: 24,
+  end: null
+}
 
-  app.use('/api/corpora/testfile/:word', (req, res) => {
-    const nextWords = test.get(req.params.word)
-    res.json(nextWords.slice(0, 3))
-  })
+function createRoutes(file){
+  readAndBuildTries(file.location, file.start, file.end, 1, trie => {
+    console.log(`Loaded ${file.name}`)
 
-  app.use('/api/corpora/testfile', (req, res) => {
-    res.json(test.get().slice(0, 3))
+    app.use(`/api/corpora/${file.name}/:word`, (req, res) => {
+      const nextWords = trie.get(req.params.word)
+      res.json(nextWords.slice(0, 3))
+    })
+  
+    app.use(`/api/corpora/${file.name}`, (req, res) => {
+      res.json(trie.get().slice(0, 3))
+    })  
   })
-})
+}
+
+createRoutes(testFile)
 
 module.exports = app
