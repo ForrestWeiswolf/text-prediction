1c9411763aeaca56429c839ef740cb0937f255f7
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-25 10:14:15 -0400

    Move createRoutesFromFile to fsUtils.js

--- src/server/fsUtils.js
+++ src/server/fsUtils.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const WordTries = require('build-word-tries')
 
-function readAndBuildTries(filename, start=0, end=null, depth, callback) {
+function readAndBuildTries(filename, start = 0, end = null, depth, callback) {
   fs.readFile(
     path.join(__dirname, filename),
     { encoding: 'utf-8' },
@@ -18,4 +18,25 @@ function readAndBuildTries(filename, start=0, end=null, depth, callback) {
   )
 }
 
-module.exports = { readAndBuildTries }
+function createRoutesFromFile(file, app) {
+  readAndBuildTries(
+    `./corpora/${file.name}.txt`,
+    file.start,
+    file.end,
+    1,
+    trie => {
+      console.log(`Loaded ${file.name}`)
+
+      app.use(`/api/corpora/${file.name}/:word`, (req, res) => {
+        const nextWords = trie.get(req.params.word)
+        res.json(nextWords.slice(0, 3))
+      })
+
+      app.use(`/api/corpora/${file.name}`, (req, res) => {
+        res.json(trie.get().slice(0, 3))
+      })
+    }
+  )
+}
+
+module.exports = { readAndBuildTries, createRoutesFromFile }
