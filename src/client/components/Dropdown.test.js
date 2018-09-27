863e050b7017d26b3f9f2b105b66f62379f5b626
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-09-27 10:46:50 -0400

    Prettify Dropdown.test

--- src/client/components/Dropdown.test.js
+++ src/client/components/Dropdown.test.js
@@ -4,26 +4,26 @@ import { expect } from 'chai'
 import { Dropdown } from './Dropdown.jsx'
 
 describe('Dropdown', () => {
-	const testCorpora = ['foo', 'bar']
-	let dropdown
+  const testCorpora = ['foo', 'bar']
+  let dropdown
   beforeEach(() => {
-    dropdown = shallow(<Dropdown corpora={testCorpora}/>)
+    dropdown = shallow(<Dropdown corpora={testCorpora} />)
   })
 
   it('Renders a <select></select>', () => {
-		expect(dropdown.find('select').length).to.equal(1)
-	})
+    expect(dropdown.find('select').length).to.equal(1)
+  })
 
-	describe('select', () => {
-		let select
-		beforeEach(() => {
-			select = dropdown.find('select')
-		})
+  describe('select', () => {
+    let select
+    beforeEach(() => {
+      select = dropdown.find('select')
+    })
 
-		it('has an option for each corpus passed in props', () => {
-			testCorpora.forEach(c => {
-				expect(select.find(`option[value="${c}"]`).length).to.equal(1)
-			})
-		})
-	})
+    it('has an option for each corpus passed in props', () => {
+      testCorpora.forEach(c => {
+        expect(select.find(`option[value="${c}"]`).length).to.equal(1)
+      })
+    })
+  })
 })
