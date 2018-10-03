132e199ab53618eb43c5f51c3878ba9e627892b0
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-10-03 10:53:48 -0400

    Server uses async await

--- src/server/fsUtils.js
+++ src/server/fsUtils.js
@@ -1,42 +1,35 @@
 const fs = require('fs')
 const path = require('path')
+const util = require('util')
 const WordTries = require('build-word-tries')
 
-function readAndBuildTries(filename, start = 0, end = null, depth, callback) {
-  fs.readFile(
-    path.join(__dirname, filename),
-    { encoding: 'utf-8' },
-    (err, data) => {
-      if (err) {
-        throw err
-      } else {
-        const text = data.slice(start, end || data.length)
-        const tries = new WordTries(text, depth)
-        callback(tries)
-      }
-    }
-  )
+const promisifiedReadFile = util.promisify(fs.readFile)
+
+async function readAndBuildTries(filename, start = 0, end = null, depth) {
+  const file = await promisifiedReadFile(path.join(__dirname, filename), {
+    encoding: 'utf-8',
+  })
+
+  const tries = new WordTries(file.slice(start, end || data.length), depth)
+  return tries
 }
 
-function createRoutesFromFile(file, app) {
-  readAndBuildTries(
+async function createRoutesFromFile(file, app) {
+  const tries = await readAndBuildTries(
     `./corpora/${file.filename}.txt`,
     file.start,
     file.end,
-    1,
-    trie => {
-      console.log(`Loaded ${file.name}`)
-
-      app.use(`/api/corpus/${file.filename}/:word`, (req, res) => {
-        const nextWords = trie.get(req.params.word)
-        res.json(nextWords.slice(0, 3))
-      })
-
-      app.use(`/api/corpus/${file.filename}`, (req, res) => {
-        res.json(trie.get().slice(0, 3))
-      })
-    }
+    1
   )
+
+  app.use(`/api/corpus/${file.filename}/:word`, (req, res) => {
+    const nextWords = tries.get(req.params.word)
+    res.json(nextWords.slice(0, 3))
+  })
+
+  app.use(`/api/corpus/${file.filename}`, (req, res) => {
+    res.json(tries.get().slice(0, 3))
+  })
 }
 
 module.exports = { readAndBuildTries, createRoutesFromFile }
