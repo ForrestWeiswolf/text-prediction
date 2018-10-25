1a5107580309c041b0c60e015d470dd5ee87d299
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2018-10-25 09:57:59 -0400

    Server accepts multiple words in query

--- src/server/fsUtils.js
+++ src/server/fsUtils.js
@@ -10,25 +10,28 @@ async function readAndBuildTries(filename, start = 0, end = null, depth) {
     encoding: 'utf-8',
   })
 
-  const tries = new WordTries(file.slice(start, end || data.length), depth)
+  const tries = new WordTries(file.slice(start, end || file.length), depth)
   return tries
 }
 
 async function createRoutesFromFile(file, app) {
   const tries = await readAndBuildTries(
     `./corpora/${file.filename}.txt`,
-    file.start,
-    file.end,
-    1
+    31337,
+    270920,
+    2
   )
 
-  app.use(`/api/corpus/${file.filename}/:word`, (req, res) => {
-    const nextWords = tries.get(req.params.word)
-    res.json(nextWords.slice(0, 3))
-  })
-
-  app.use(`/api/corpus/${file.filename}`, (req, res) => {
-    res.json(tries.get().slice(0, 3))
+  app.use(`/api/corpus/${file.filename}/`, (req, res) => {
+    let lastWords
+    try {
+      lastWords = req.query.words ? JSON.parse(req.query.words) : []
+      const nextWords = tries.get(...lastWords)
+      res.json(nextWords.slice(0, 3))
+    } catch (error) {
+      console.error(error)
+      res.end()
+    }
   })
 }
 
