eb53f5baf23675e197c4ee2b4ed8a6c350697fd0
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-01-08 11:41:03 -0500

    Use preact-compat

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -90,10 +90,13 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      // use Preact instead of React
+      "react": "preact-compat",
+      "react-dom": "preact-compat",
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
