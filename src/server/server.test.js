8c054f9f34af77b760b916bd5ebe9120e6bd6a27
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-04-10 09:58:06 -0400

    Remove some useless 'tests'

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -3,38 +3,8 @@ const server = require('./server.js')
 
 const agent = supertest.agent(server)
 
-xdescribe('server', () => {
+describe('server', () => {
   describe('GET /api/corpora', () => {
-    xit('just fails', () => {
-      // expect.assertions(1)
-      const promise = new Promise((resolve, reject) => {
-        try {
-          resolve('foo')
-        } catch(e) {
-          reject(e)
-        }
-      })
-
-      promise.then(res => {
-        expect(res).toEqual('bar')
-      })
-    })
-
-    xit('just passes', () => {
-      // expect.assertions(1)
-      const promise = new Promise((resolve, reject) => {
-        try {
-          resolve('foo')
-        } catch(e) {
-          reject(e)
-        }
-      })
-
-      promise.then(res => {
-        expect(res).toEqual('foo')
-      })
-    })
-
     it('sends a response', () => {
       agent
         .get('/api/corpora')
