981b159f76b6b35fb9929d019cfc61b4d88bb2c0
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-04-10 09:58:35 -0400

    Make server tests wait for server to start

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -4,6 +4,13 @@ const server = require('./server.js')
 const agent = supertest.agent(server)
 
 describe('server', () => {
+  beforeAll(async () => {
+    // wait for the server to start
+    // TODO: give the server a health endpoint to check here
+    // so that we can start when it is ready instead of using a hardcoded guess of how long that takes
+    await new Promise(resolve => setTimeout(resolve, 500))
+  })
+
   describe('GET /api/corpora', () => {
     it('sends a response', () => {
       agent
