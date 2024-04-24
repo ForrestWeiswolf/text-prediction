dd5028c173aa774197b907e4f4b3d7a21fe5c06c
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-04-24 09:30:03 -0400

    Reorganize backend

--- src/server/fsUtils.js
+++ src/server/fsUtils.js
@@ -3,45 +3,21 @@ const path = require('path')
 const util = require('util')
 const WordTries = require('build-word-tries')
 
+const DEPTH = 2
+
 const promisifiedReadFile = util.promisify(fs.readFile)
 
-async function readAndBuildTries(filename, start = 0, end = null, depth) {
-  const file = await promisifiedReadFile(path.join(__dirname, filename), {
+async function buildTriesFromFile(corpus) {
+  const filePath = `./corpora/${corpus.filename}.txt`
+  const file = await promisifiedReadFile(path.join(__dirname, filePath), {
     encoding: 'utf-8',
   })
 
   console.log('creating tries')
-  const tries = new WordTries(file.slice(start, end || file.length), depth)
-  console.log(`loaded ${filename}`)
+  const tries = new WordTries(file.slice(corpus.start || 0, corpus.end || file.length), DEPTH)
+  console.log(`loaded ${corpus.filename}`)
+
   return tries
 }
 
-async function createRoutesFromFile(file, app) {
-  console.log('creating routes from', file.name)
-  const tries = await readAndBuildTries(
-    `./corpora/${file.filename}.txt`,
-    file.start,
-    file.end,
-    2
-  )
-
-  app.use(`/api/corpus/${file.filename}/`, (req, res) => {
-    let lastWords
-    try {
-      // http://localhost:8080/api/corpus/beowulf?words=[%22if%22,%22this%22]
-      lastWords = req.query.words ? JSON.parse(req.query.words) : []
-      console.log(lastWords)
-      lastWords = lastWords.map(w => w.toLowerCase())
-      let nextWords = tries.get(...lastWords)
-      if(nextWords.length === 0){
-        nextWords = tries.get(lastWords[lastWords.length - 1])
-      }
-      res.json(nextWords.slice(0, 3))
-    } catch (error) {
-      console.error(error)
-      res.end()
-    }
-  })
-}
-
-module.exports = { readAndBuildTries, createRoutesFromFile }
+module.exports = { buildTriesFromFile }
--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,36 @@
 const express = require('express')
-const { createRoutesFromFile } = require('./fsUtils.js')
+const { buildTriesFromFile } = require('./fsUtils.js')
 const list = require('./corpora/list.json')
 
 const app = express()
 
-list.forEach(file => {
-  createRoutesFromFile(file, app)
+list.forEach(async corpus => {
+  console.log('creating routes from', corpus.name)
+  const tries = await buildTriesFromFile(corpus)
+
+  app.use(`/api/corpus/${corpus.filename}/`, (req, res) => {
+    let lastWords
+    try {
+      // http://localhost:8080/api/corpus/beowulf?words=[%22if%22,%22this%22]
+      lastWords = req.query.words ? JSON.parse(req.query.words) : []
+      console.log(lastWords)
+      lastWords = lastWords.map(w => w.toLowerCase())
+      let nextWords = tries.get(...lastWords)
+      if (nextWords.length === 0) {
+        nextWords = tries.get(lastWords[lastWords.length - 1])
+      }
+      res.json(nextWords.slice(0, 3))
+    } catch (error) {
+      console.error(error)
+      res.end()
+    }
+  })
 })
 
 app.get('/api/corpora', (req, res) => {
   res.send(
-    list.map(file => {
-      return { name: file.name, route: file.filename }
+    list.map(corpus => {
+      return { name: corpus.name, route: corpus.filename }
     })
   )
 })
