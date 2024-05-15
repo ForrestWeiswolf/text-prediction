d7295f65d6d5722e9a2748aa6384899e01d53159
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-05-15 10:17:53 -0400

    Don't re-fetch corpora that are already present

--- fetchCorpora.js
+++ fetchCorpora.js
@@ -1,7 +1,17 @@
 const list = require('./src/server/corpora/list.json')
-const {writeFile} = require('node:fs/promises')
+const { writeFile, readdir } = require('node:fs/promises')
 
-list.forEach(async ({source, filename}) => {
-  const response = await fetch(source)
-  writeFile(`./src/server/corpora/${filename}.txt`, response.body)
-})
+const fetchCorpora = async () => {
+  const corpora = await readdir('./src/server/corpora')
+
+  console.log(corpora)
+  list.forEach(async ({ source, filename }) => {
+    if (!corpora.includes(`${filename}.txt`)) {
+      console.log(`Fetching ${filename}`)
+      const response = await fetch(source)
+      writeFile(`./src/server/corpora/${filename}.txt`, response.body)
+    }
+  })
+}
+
+fetchCorpora()
