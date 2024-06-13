998bd45d494e6c2cb7e2abb0688270b452436c57
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-06-13 11:31:12 -0400

    WIP: make SuggestionContainer a function component

--- src/client/components/SuggestionContainer.jsx
+++ src/client/components/SuggestionContainer.jsx
@@ -1,42 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { debounce, isEqual } from 'lodash'
+import { debounce } from 'lodash'
 import SuggestionBox from './SuggestionBox.jsx'
 import { fetchSuggestions } from '../store/index.js'
 
-export class SuggestionContainer extends Component {
-  constructor(props) {
-    super()
-  }
-
-  componentDidMount() {
-    if (this.props.selectedCorpus) {
-      this.props.fetchSuggestions(this.props.lastWords)
+export const SuggestionContainer = (props) => {
+  React.useEffect(() => {
+    console.log(props.lastWords)
+    if (props.selectedCorpus) {
+      fetchSuggestions(props.lastWords)
     }
-  }
+  }, [props.selectedCorpus, JSON.stringify(props.lastWords)])
 
-  componentDidUpdate(prevProps) {
-    const changedWords = !isEqual(prevProps.lastWords, this.props.lastWords)
-    const changedCorpus = prevProps.selectedCorpus !== this.props.selectedCorpus
 
-    if (this.props.selectedCorpus && (changedWords || changedCorpus)) {
-      this.props.fetchSuggestions(this.props.lastWords)
-    }
-  }
-
-  render() {
-    const suggestions = this.props.suggestions
-
-    return (
-      <div>
-        {!suggestions.map && console.log(suggestions)}
-        {suggestions && suggestions.map((suggestion, idx) => (
-          <SuggestionBox value={suggestion} key={idx} />
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {!props.suggestions.map && console.log(props.suggestions)}
+      {props.suggestions && props.suggestions.map((suggestion, idx) => (
+        <SuggestionBox value={suggestion} key={idx} />
+      ))}
+    </div>
+  )
 }
 
 SuggestionContainer.propTypes = {
