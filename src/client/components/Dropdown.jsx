365141a4f1afd076f96de7f727b1f0a4c9c224c7
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-06-24 10:56:21 -0400

    Rename

--- src/client/components/Dropdown.jsx
+++ src/client/components/Dropdown.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { fetchCorpora, selectCorpus } from '../store/index.js'
