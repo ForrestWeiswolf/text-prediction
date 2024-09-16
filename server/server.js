b8ea7380260d1003ddb5ce56733c4b8b58452fbb
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-09-16 17:54:36 -0400

    Revert "Avoid using top level await during server setup"
    This reverts commit f27088da06851688d6e820b1488c654bf08a35e7.

--- server/server.js
+++ server/server.js
@@ -4,37 +4,37 @@ const list = require('./list.json')
 
 const app = express()
 
-fetchCorpora().then(() => {
-  list.forEach(async corpus => {
-    console.log('creating routes from', corpus.name)
-    const tries = await buildTriesFromFile(corpus)
+await fetchCorpora()
 
-    app.use(`/api/corpus/${corpus.filename}/`, (req, res) => {
-      let lastWords
-      try {
-        // http://localhost:8080/api/corpus/beowulf?words=[%22if%22,%22this%22]
-        lastWords = req.query.words ? JSON.parse(req.query.words) : []
-        console.log(lastWords)
-        lastWords = lastWords.map(w => w.toLowerCase())
-        let nextWords = tries.get(...lastWords)
-        if (nextWords.length === 0) {
-          nextWords = tries.get(lastWords[lastWords.length - 1])
-        }
-        res.json(nextWords.slice(0, 3))
-      } catch (error) {
-        console.error(error)
-        res.end()
+list.forEach(async corpus => {
+  console.log('creating routes from', corpus.name)
+  const tries = await buildTriesFromFile(corpus)
+
+  app.use(`/api/corpus/${corpus.filename}/`, (req, res) => {
+    let lastWords
+    try {
+      // http://localhost:8080/api/corpus/beowulf?words=[%22if%22,%22this%22]
+      lastWords = req.query.words ? JSON.parse(req.query.words) : []
+      console.log(lastWords)
+      lastWords = lastWords.map(w => w.toLowerCase())
+      let nextWords = tries.get(...lastWords)
+      if (nextWords.length === 0) {
+        nextWords = tries.get(lastWords[lastWords.length - 1])
       }
-    })
+      res.json(nextWords.slice(0, 3))
+    } catch (error) {
+      console.error(error)
+      res.end()
+    }
   })
+})
 
-  app.get('/api/corpora', (req, res) => {
-    res.send(
-      list.map(corpus => {
-        return { name: corpus.name, route: corpus.filename }
-      })
-    )
-  })
+app.get('/api/corpora', (req, res) => {
+  res.send(
+    list.map(corpus => {
+      return { name: corpus.name, route: corpus.filename }
+    })
+  )
 })
 
 module.exports = app
