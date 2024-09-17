715e55764f4615b0195b1ceece7fafc9a0d8a89d
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-09-17 18:38:25 -0400

    Make corpus dir an environment variable

--- server/fetchCorpora.js
+++ server/fetchCorpora.js
@@ -3,21 +3,21 @@ const { writeFile, readdir, mkdir } = require('node:fs/promises')
 
 const fetchCorpora = async () => {
   try {
-    await mkdir('./corpora')
+    await mkdir(`${process.env.CORPUS_DIR}/corpora`)
   } catch (err) {
     if (err.code !== 'EEXIST') {
       throw err
     }
   }
 
-  const corpora = await readdir('./corpora')
+  const corpora = await readdir(`${process.env.CORPUS_DIR}/corpora`)
 
   console.log(corpora)
   list.forEach(async ({ source, filename }) => {
     if (!corpora.includes(`${filename}.txt`)) {
       console.log(`Fetching ${filename}`)
       const response = await fetch(source)
-      writeFile(`./corpora/${filename}.txt`, response.body)
+      writeFile(`${process.env.CORPUS_DIR}/corpora/${filename}.txt`, response.body)
     }
   })
 }
