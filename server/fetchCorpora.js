f4ef56399d84d61e5bf55ab2bcf403e9692b0756
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-09-22 11:53:38 -0400

    Fix paths

--- server/fetchCorpora.js
+++ server/fetchCorpora.js
@@ -1,15 +1,15 @@
-const list = require('./src/server/list.json')
+const list = require('./list.json')
 const { writeFile, readdir } = require('node:fs/promises')
 
 const fetchCorpora = async () => {
-  const corpora = await readdir('./src/server/corpora')
+  const corpora = await readdir('./corpora')
 
   console.log(corpora)
   list.forEach(async ({ source, filename }) => {
     if (!corpora.includes(`${filename}.txt`)) {
       console.log(`Fetching ${filename}`)
       const response = await fetch(source)
-      writeFile(`./src/server/corpora/${filename}.txt`, response.body)
+      writeFile(`./corpora/${filename}.txt`, response.body)
     }
   })
 }
